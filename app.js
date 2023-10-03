0947d4e9d5ac7dff64654ace10788e092baa9031
Yaser Aly <yaser-aly@live.com>
2023-10-02 22:28:52 -0700

    installed axios and nodemon

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const { capitalizeWords, makeMoney } = require('./utils');
-const Person = require('./Person');
+const axios = require('axios');
 
-console.log(capitalizeWords('Hello World'));
-console.log(makeMoney(100));
+async function getPost() {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
+  console.log(res.data);
+}
 
-const Person1 = new Person('john', 30);
-Person1.greet();
+getPost();
